d5f0440996bc1be3f24b042b0a61f7843ab29733
Alsandz <79601736+Alsandz@users.noreply.github.com>
2021-03-03 12:17:45 +0100

    Añado documentación.

---
+++ Producto.java
@@ -0,0 +1,91 @@
+
+/**
+ * 
+ * @author alberto
+ * @version 1.0
+ * @since 24/02/21
+ *
+ */
+
+/**
+ * <h1>Clase que devuelve el resultado de multiplicar distintos números.</h1>
+ * @param multiplo es el primer número que vamos a multiplicar. 
+ * @param multiplo2 es el segundo. Al ser una multiplicación, el orden no importa.
+ * @param resultado es la solución de la operación.
+ * @return otorga la solución recién obtenida
+ */
+public class Producto extends Principal {
+	
+	
+	/**
+	 * <h1>Casos especiales:</h1>
+ *  <ul>
+ *     <li>Si uno de los parámetros es cero, el resultado será cero.
+ *     <li>Si la multiplicación resulta demasiado alta, informaremos al usuario.
+ *     <li>Si se multiplicaran dos números negativos, saldrían  positivos
+ *     <li>
+ *  </ul>
+	 * <h1>Método diseñado para multiplicar dos números reales.</h1>
+	 * @param multiplo es el primer número que vamos a multiplicar. 
+	 * @param multiplo2 es el segundo. Al ser una multiplicación, el orden no importa.
+	 * @param resultado es la solución de la operación.
+	 * @return otorga la solución recién obtenida
+	 */
+	public static double ProductoVariedad1() {
+		
+	}
+	
+	/**
+	 *  * <h1>Casos especiales:</h1>
+ *  <ul>
+ *     <li>Si uno de los parámetros es cero, el resultado será cero.
+ *     <li>Si la multiplicación resulta demasiado alta, informaremos al usuario.
+ *     <li>Si se multiplicaran dos números negativos, saldrían  positivos
+ *     <li>
+ *  </ul>
+	 * <h1>Método diseñado para multiplicar dos números enteros.</h1>
+	 * @param multiplo es el primer número que vamos a multiplicar. 
+	 * @param multiplo2 es el segundo. Al ser una multiplicación, el orden no importa.
+	 * @param resultado es la solución de la operación.
+	 * @return otorga la solución recién obtenida
+	 */
+    public static double ProductoVariedad2() {
+    	
+}
+    
+	/**
+	 *  * <h1>Casos especiales:</h1>
+ *  <ul>
+ *     <li>Si uno de los parámetros es cero, el resultado será cero.
+ *     <li>Si la multiplicación resulta demasiado alta, informaremos al usuario.
+ *     <li>Si se multiplicaran tres números negativos, saldrían  negativos
+ *     <li>
+ *  </ul>
+	 * <h1>Método diseñado para multiplicar tres números reales.</h1>
+	 * @param multiplo es el primer número que vamos a multiplicar. 
+	 * @param multiplo2 es el segundo. Al ser una multiplicación, el orden no importa.
+	 * @param resultado es la solución de la operación.
+	 * @return otorga la solución recién obtenida
+	 */
+    public static double ProductoVariedad3() {
+
+	}
+    
+	/**
+	 *  * <h1>Casos especiales:</h1>
+ *  <ul>
+ *     <li>Si la base es cero, el resultado será cero.
+ *     <li>Si la multiplicación resulta demasiado alta, informaremos al usuario.
+ *     <li>Si el exponente es cero, el resultado será uno.
+ *     <li>Si el exponente es un medio, se calculará la raíz cuadrada de la base.
+ *  </ul>
+	 * <h1>Método diseñado para calcular la potencia de un número real elevado a otro.</h1>
+	 * @param multiplo es el primer número que vamos a multiplicar. 
+	 * @param multiplo2 es el segundo. Al ser una multiplicación, el orden no importa.
+	 * @param resultado es la solución de la operación.
+	 * @return otorga la solución recién obtenida
+	 */
+    public static double ProductoVariedad4() {
+ 
+	}
+}
