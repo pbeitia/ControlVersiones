9f31383acba501a49ef3ce0981e64a777cc1489c
pbeitia <78168656+pbeitia@users.noreply.github.com>
2021-03-03 20:00:16 +0100

    Cambio en documentacion

--- Suma.java
+++ Suma.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 /**
  * Esta clase pertenece a la operaci�n suma de la calculadora
  * @author Grupo 
- * @version 2.0
+ * @version 3.0
  * @since 22/02/2021
  */
 public class Suma extends Principal{
 	static Scanner leer = new Scanner(System.in);
 	
-	/**
-	 * M�todo que suma dos n�meros reales, con dos par�metros de entrada (num1 y num2) y un par�metro de salida (resultado).
+/**
+	 * Método que suma dos números reales, con dos parámetros de entrada (num1 y num2) y un parámetro de salida (resultado).
 	 * 
 	 *Casos Especiales:
 	 *
 	 * 	<ul>
-	 *		<li>Si alg�n n�mero es negativo, la suma se realiza igual.
-	 *		<li>Si alg�n n�mero es 0, la suma se realiza de la misma forma.
-	 *		<li>Si alg�n n�mero es decimal, la suma se realiza de la misma forma.
+	 *		<li>Si algun numero es negativo, la suma se realiza igual.
+	 *		<li>Si algun numero es 0, la suma se realiza de la misma forma.
+	 *		<li>Si algun numero es decimal, la suma se realiza de la misma forma.
+	 *		<li>Si algun numero es demasiado alto da error.
 	 *	</ul>
 	 *
-	 * @param num1 Primer n�mero de tipo real para operar en la suma
-	 * @param num2 Segundo n�mero de tipo real  para operar en la suma
-	 * @param resultado Resultado final de la suma de los dos n�meros
+	 * @param num1 Primer numero de tipo real para operar en la suma
+	 * @param num2 Segundo numero de tipo real  para operar en la suma
+	 * @param resultado Resultado final de la suma de los dos nï¿½meros
 	 * @return Devuelve el resultado
 	 * 
 	 */
@@ -35,23 +36,25 @@ public class Suma extends Principal{
 		resultado = num1 + num2;
 		return resultado;
 	}
-	
+
 	/**
-	 * M�todo que suma dos n�meros enteros, con dos par�metros de entrada (num1 y num2) y un par�metro de salida (resultado).
+	 * Metodo que suma 3 numeros reales, con 3 parametros de entrada (num1, num2, num3) y un parametro de salida (resultado).
 	 * 
 	 *Casos Especiales:
 	 *
 	 * 	<ul>
-	 *		<li>Si alg�n n�mero es negativo, la suma se realiza igual.
-	 *		<li>Si alg�n n�mero es 0, la suma se realiza de la misma forma.
-	 *		<li>Si alg�n n�mero es decimal, la suma no se realizar� porque es con n�meros enteros.
+	 *		<li>Si algun numero es negativo, la suma se realiza igual.
+	 *		<li>Si algun numero es 0, la suma se realiza de la misma forma.
+	 *		<li>Si algun numero es decimal, la suma se realizaria de la misma forma.
 	 *	</ul>
 	 *
-	 * @param num1 Primer n�mero de tipo entero para operar en la suma
-	 * @param num2 Segundo n�mero de tipo entero para operar en la suma
-	 * @param resultado Resultado final de la suma de los dos n�meros
+	 * @param num1 Primer numero real para operar en la suma
+	 * @param num2 Segundo numero real  para operar en la suma
+	 * @param num3 Tercer numero real para operar en la suma
+	 * @param resultado Variable que almacena el resultado de la suma
 	 * @return
 	 */
+
 	public static int sumaTipoDos() {
 		System.out.println("Introduce el primer n�mero: ");
 		int num1 = leer.nextInt();
@@ -61,23 +64,22 @@ public class Suma extends Principal{
 		return resultado;
 	}
 	
-	/**
-	 * M�todo que suma 3 n�meros reales, con 3 par�metros de entrada (num1, num2, num3) y un par�metro de salida (resultado).
+/**
+	 * Metodo que acumula el valor del numero que escribas. Tiene 1 parametro de entrada (num) y un acumulador que almacena el valor.
 	 * 
 	 *Casos Especiales:
 	 *
 	 * 	<ul>
-	 *		<li>Si alg�n n�mero es negativo, la suma se realiza igual.
-	 *		<li>Si alg�n n�mero es 0, la suma se realiza de la misma forma.
-	 *		<li>Si alg�n n�mero es decimal, la suma se realizar� de la misma forma.
+	 *		<li>Si algun numero es negativo, se sumaria al acumulador.
+	 *		<li>Si algun numero es 0, sumaria al acumulador.
+	 *		<li>Si algun numero es decimal, sumaria al acumulador.
 	 *	</ul>
 	 *
-	 * @param num1 Primer n�mero real para operar en la suma
-	 * @param num2 Segundo n�mero real  para operar en la suma
-	 * @param num3 Tercer n�mero real para operar en la suma
-	 * @param resultado Variable que almacena el resultado de la suma
-	 * @return
+	 * @param num Numero que mete el usuario y se acumula.
+	 * @param acumulador Variable encargada de acumular los numeros introducidos.
+	 * @return Devuelve el acumulador
 	 */
+	 
 	public static double sumaTipoTres() {
 		System.out.println("Introduce el primer n�mero: ");
 		double num1 = leer.nextDouble();
