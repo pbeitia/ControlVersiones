3bd95b35a566a529f86ddd21ed680f73b9ef97c7
pbeitia <78168656+pbeitia@users.noreply.github.com>
2021-03-03 13:15:46 +0100

    Añado la prueba de la Suma y Cociente

--- Principal.java
+++ Principal.java
@@ -1,9 +1,13 @@
 public class Principal {
 
 	public static void main(String[] args) {
-
+		System.out.println("-INICIO SUMA-");
+		//Soy el Grupo, realizo prueba de Suma
+		System.out.println(Suma.sumaTipoTres());
 		
-
+		System.out.println("-INICIO COCIENTE-");
+		//Soy Pablo, realizo la prueba de Cociente
+		System.out.println(Cociente.CocienteTipoUno());
 		}
 	}
 
