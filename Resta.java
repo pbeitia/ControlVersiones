2a022a441cea13e5833bee290569616f0a47a74e
xel97 <hughost97@gmail.com>
2021-03-03 12:30:38 +0100

    añado documentación

--- Resta.java
+++ Resta.java
@@ -1,6 +1,69 @@
 
 
-public class Resta extends main {
+/**
+ * Esta clase pertenece a la operacion resta de la calculadora
+ * @author Jaime Hebrero
+ *@version 1.0
+ *@since 24/02/2021
+ */
+
+
+public class Resta extends Principal {
 	
 	
+	
+	/**
+	 * M�todo para restar 2 n�meros reales, tiene 2 parametros de entrada y uno de salida (la soluci�n).
+	 * @param num1 Primer parametro para operar la resta.
+	 * @param num2 Segundo parametro para operar la resta.
+	 * @param resultado Resultado de la resta entre los 2 par�metros.
+	 * @return Devuelve resultado.
+	 */
+	
+	public static double restaTipoUno(){
+		
+		
+		return resultado;
+	}
+	/**
+	 * M�todo para restar 2 n�meros enteros, tiene 2 parametros de entrada y uno de salida (la soluci�n).
+	 * @param num1 Primer parametro para operar la resta.
+	 * @param num2 Segundo parametro para operar la resta.
+	 * @param resultado Resultado de la resta entre los 2 par�metros.
+	 * @return Devuelve resultado.
+	 */
+		
+	public static int restaTipoDos(){
+		
+	}
+			
+	/**
+	 * M�todo para restar 3 n�meros enteros, tiene 3 parametros de entrada y uno de salida (la soluci�n).
+	 * @param num1 Primer parametro para operar la resta.
+	 * @param num2 Segundo parametro para operar la resta.
+	 * @param num3 Tercer parametro para operar en la resta.
+	 * @param resultado Resultado de la resta entre los 2 par�metros.
+	 * @return Devuelve resultado.
+	 */
+		
+	public static double restaTipoTres(){
+		
+	}
+	/**
+	 * M�todo para acumular un numero que escribamos. Tiene un parametro de entrada y un parametro acumulador al que se le suma el parametro de entrada.
+	 * @param num parametro de entrada.
+	 * @param acumulador parametro donde se van restando los numeros ingresados.
+	 * @return Devuelve resultado.
+	 */
+	public static double restaTipoCuatro(){
+		
+		return acumulador;
+	}
+	/**
+	 *  Casos especiales:
+	 *  <ul>
+	 *  	<li> Si se resta un n�mero negativo este se suma.
+	 *  	<li> si se resta 0 no cambia nada.
+	 *  <ul>
+	 */
 }
