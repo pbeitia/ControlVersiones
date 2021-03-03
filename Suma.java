fd24ee2375286348790104b34b17f859896c382d
pbeitia <78168656+pbeitia@users.noreply.github.com>
2021-03-03 12:43:20 +0100

    Añadimos la documentación

---
+++ Suma.java
@@ -0,0 +1,90 @@
+/**
+ * Esta clase pertenece a la operaci�n suma de la calculadora
+ * @author Grupo 
+ * @version 1.0
+ * @since 22/02/2021
+ */
+ 
+public class Suma extends Principal{
+	/**
+	 * M�todo que suma dos n�meros reales, con dos par�metros de entrada (num1 y num2) y un par�metro de salida (resultado).
+	 * 
+	 *Casos Especiales:
+	 *
+	 * 	<ul>
+	 *		<li>Si alg�n n�mero es negativo, la suma se realiza igual.
+	 *		<li>Si alg�n n�mero es 0, la suma se realiza de la misma forma.
+	 *		<li>Si alg�n n�mero es decimal, la suma se realiza de la misma forma.
+	 *	</ul>
+	 *
+	 * @param num1 Primer n�mero de tipo real para operar en la suma
+	 * @param num2 Segundo n�mero de tipo real  para operar en la suma
+	 * @param resultado Resultado final de la suma de los dos n�meros
+	 * @return Devuelve el resultado
+	 * 
+	 */
+	public static double sumaTipoUno() {
+
+	}
+	
+	/**
+	 * M�todo que suma dos n�meros enteros, con dos par�metros de entrada (num1 y num2) y un par�metro de salida (resultado).
+	 * 
+	 *Casos Especiales:
+	 *
+	 * 	<ul>
+	 *		<li>Si alg�n n�mero es negativo, la suma se realiza igual.
+	 *		<li>Si alg�n n�mero es 0, la suma se realiza de la misma forma.
+	 *		<li>Si alg�n n�mero es decimal, la suma no se realizar� porque es con n�meros enteros.
+	 *	</ul>
+	 *
+	 * @param num1 Primer n�mero de tipo entero para operar en la suma
+	 * @param num2 Segundo n�mero de tipo entero para operar en la suma
+	 * @param resultado Resultado final de la suma de los dos n�meros
+	 * @return
+	 */
+	public static int sumaTipoDos() {
+
+	}
+	
+	/**
+	 * M�todo que suma 3 n�meros reales, con 3 par�metros de entrada (num1, num2, num3) y un par�metro de salida (resultado).
+	 * 
+	 *Casos Especiales:
+	 *
+	 * 	<ul>
+	 *		<li>Si alg�n n�mero es negativo, la suma se realiza igual.
+	 *		<li>Si alg�n n�mero es 0, la suma se realiza de la misma forma.
+	 *		<li>Si alg�n n�mero es decimal, la suma se realizar� de la misma forma.
+	 *	</ul>
+	 *
+	 * @param num1 Primer n�mero real para operar en la suma
+	 * @param num2 Segundo n�mero real  para operar en la suma
+	 * @param num3 Tercer n�mero real para operar en la suma
+	 * @param resultado Variable que almacena el resultado de la suma
+	 * @return
+	 */
+	public static double sumaTipoTres() {
+
+	}
+	
+	/**
+	 * M�todo que acumula el valor del n�mero que escribas. Tiene 1 par�metro de entrada (num) y un acumulador que almacena el valor.
+	 * 
+	 *Casos Especiales:
+	 *
+	 * 	<ul>
+	 *		<li>Si alg�n n�mero es negativo, se sumar�a al acumulador.
+	 *		<li>Si alg�n n�mero es 0, se sumar�a al acumulador.
+	 *		<li>Si alg�n n�mero es decimal, se sumar�a al acumulador.
+	 *	</ul>
+	 *
+	 * @param num Número que añade el usuario y se acumula.
+	 * @param acumulador Variable encargada de acumular los números introducidos.
+	 * @return
+	 */
+	public static double sumaTipoCuatro() {
+
+	}
+	
+}
